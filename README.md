590ced54404e550294a5810dd1b1f56af8e8d045
eduardo15686 <39798800+eduardo15686@users.noreply.github.com>
2019-06-12 07:52:30 -0500

    n
    nuevo proyecto

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # HotelHost
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.4.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.0.2.
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,58 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {
-  MatButtonModule,
-  MatCardModule,
-  MatDialogModule,
-  MatDividerModule, MatExpansionModule,
-  MatFormFieldModule,
-  MatInputModule, MatPaginatorModule, MatProgressSpinnerModule,
-  MatToolbarModule,
-  MatGridListModule,
-  MatMenuTrigger,
-  MatSelectModule,
-  MatNativeDateModule
-} from '@angular/material';
-import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    InicioComponent
+    AppComponent
   ],
   imports: [
-    HttpClientModule,
     BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    ReactiveFormsModule,
-    AppRoutingModule,
-    MatCardModule,
-    MatDividerModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatExpansionModule,
-    MatProgressSpinnerModule,
-    MatPaginatorModule,
-    MatDialogModule,
-    MatGridListModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
-    MatMenuModule,
-    MatDatepickerModule,
-    MatNativeDateModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
