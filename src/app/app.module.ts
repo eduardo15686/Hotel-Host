f453e20a21ac9bce419c8c763a62213aceed437c
eduardo15686 <39798800+eduardo15686@users.noreply.github.com>
2019-06-12 08:50:27 -0500

    pantalla de inicio
    se agrego el filtro por los estados de la pagina principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,54 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatDialogModule,
+  MatDividerModule, MatExpansionModule,
+  MatFormFieldModule,
+  MatInputModule, MatPaginatorModule, MatProgressSpinnerModule,
+  MatToolbarModule,
+  MatGridListModule,
+  MatMenuTrigger,
+  MatSelectModule,
+  MatNativeDateModule
+} from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    MatCardModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    MatDialogModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatMenuModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
