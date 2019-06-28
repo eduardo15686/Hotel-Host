97b16b876c7563b420156dc751cbde6d630564e0
Dkcharthas <39799675+Dkcharthas@users.noreply.github.com>
2019-06-27 20:36:22 -0500

    subir-hoteles

--- src/app/components/subir-hotel/subir-hotel.component.ts
+++ src/app/components/subir-hotel/subir-hotel.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Habitacion {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-subir-hotel',
   templateUrl: './subir-hotel.component.html',
   styleUrls: ['./subir-hotel.component.css']
 })
 export class SubirHotelComponent implements OnInit {
-
+ 
   constructor() { }
 
   ngOnInit() {
+    
   }
+
 
 }
